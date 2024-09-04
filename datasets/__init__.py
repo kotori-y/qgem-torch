ee4cedf1b0e5c88a9e2f61b1acd865681d2386bc
Kotori Y <yzjkid9@gmail.com>
2024-09-04 20:23:43 +0800

    removed unused codes and added new params

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -24,6 +24,8 @@ class EgeognnPretrainedDataset(InMemoryDataset):
             atom_names,
             bond_names,
             with_provided_3d,
+            mask_ratio,
+            mask_value,
             transform=None,
             pre_transform=None,
             remove_hs=False,
@@ -40,6 +42,9 @@ class EgeognnPretrainedDataset(InMemoryDataset):
 
         self.atom_names = atom_names
         self.bond_names = bond_names
+
+        self.mask_ratio = mask_ratio
+        self.mask_value = mask_value
 
         super().__init__(self.folder, transform, pre_transform)
         self.data, self.slices = torch.load(self.processed_paths[0])
@@ -79,7 +84,7 @@ class EgeognnPretrainedDataset(InMemoryDataset):
         graph['Ad_node_j'] = indice.T.reshape([-1, 1])
         graph['atom_distance'] = dist_matrix.reshape([-1, 1])
 
-        mask_egeognn_graph(graph, mask_ratio=0.1)
+        graph = mask_egeognn_graph(graph, mask_ratio=self.mask_ratio, mask_value=self.mask_value)
         #
         # graph['atom_cm5'] = np.array(graph.get('cm5', []))
         # graph['atom_espc'] = np.array(graph.get('espc', []))
@@ -152,6 +157,8 @@ class EgeognnPretrainedDataset(InMemoryDataset):
 
             data.node_feat = torch.from_numpy(graph["node_feat"]).to(torch.int64)
             data.edge_attr = torch.from_numpy(graph["edge_attr"]).to(torch.int64)
+            data.BondAngleGraph_edge_attr = torch.from_numpy(graph["BondAngleGraph_edge_attr"]).to(torch.float32)
+            data.AngleDihedralGraph_edge_attr = torch.from_numpy(graph["AngleDihedralGraph_edge_attr"]).to(torch.float32)
 
             data.Bl_node_i = torch.from_numpy(graph["Bl_node_i"]).to(torch.int64)
             data.Bl_node_j = torch.from_numpy(graph["Bl_node_j"]).to(torch.int64)
@@ -172,68 +179,12 @@ class EgeognnPretrainedDataset(InMemoryDataset):
             data.Ad_node_j = torch.from_numpy(graph["Ad_node_j"]).to(torch.int64)
             data.atom_distance = torch.from_numpy(graph["atom_distance"]).to(torch.float32)
 
-            # if "super_edge_index" in graph:
-            #     graph.super_edge_index = torch.from_numpy(graph["super_edge_index"]).to(torch.int64)
-            # if "supersuper_edge_index" in graph:
-            #     graph.supersuper_edge_index = torch.from_numpy(graph["supersuper_edge_index"]).to(torch.int64)
-            # if "angle_index" in graph:
-            #     graph.angle_index = torch.from_numpy(graph["angle_index"]).to(torch.int64)
-            # if "dihedral_index" in graph:
-            #     graph.dihedral_index = torch.from_numpy(graph["dihedral_index"]).to(torch.int64)
-            #
-            # graph.n_nodes = graph["n_nodes"]
-            # graph.n_edges = graph["n_edges"]
-            # if 'n_angles' in graph:
-            #     graph.n_angles = graph["n_angles"]
-            # if 'n_dihedrals' in graph:
-            #     graph.n_dihedrals = graph["n_dihedrals"]
-            #
-            # graph.pos = torch.from_numpy(mol.GetConformer(0).GetPositions()).to(torch.float)
-            #
-            # graph.rd_mol = copy.deepcopy(mol)
-            # graph.isomorphisms = isomorphic_core(mol)
-            # graph.nei_src_index = torch.from_numpy(graph["nei_src_index"]).to(torch.int64)
-            # graph.nei_tgt_index = torch.from_numpy(graph["nei_tgt_index"]).to(torch.int64)
-            # graph.nei_tgt_mask = torch.from_numpy(graph["nei_tgt_mask"]).to(torch.bool)
-            #
-            # if self.dataset_name in ['allsets']:
-            #     if "test" in subset:
-            #         test_idx.append(valid_conformation)
-            #     else:
-            #         train_idx.append(valid_conformation)
-            #
-            # else:
-            #     if "train" in subset:
-            #         train_idx.append(valid_conformation)
-            #     elif "val" in subset:
-            #         valid_idx.append(valid_conformation)
-            #     else:
-            #         test_idx.append(valid_conformation)
-            #
-            # valid_conformation += 1
             data_list.append(data)
-            #
-            # if self.pre_transform is not None:
-            #     data_list = [self.pre_transform(graph) for graph in data_list]
-            #
+
         graphs, slices = self.collate(data_list)
 
         print("Saving...")
         torch.save((graphs, slices), self.processed_paths[0])
-        # os.makedirs(os.path.join(self.root, "split"), exist_ok=True)
-        # if len(valid_idx) == 0:
-        #     valid_idx = train_idx[:6400]
-        # if len(test_idx) == 0:
-        #     test_idx = train_idx[:6400]
-        # torch.save(
-        #     {
-        #         "train": torch.tensor(train_idx, dtype=torch.long),
-        #         "valid": torch.tensor(valid_idx, dtype=torch.long),
-        #         "test": torch.tensor(test_idx, dtype=torch.long),
-        #     },
-        #     os.path.join(self.root, "split", "split_dict.pt"),
-        # )
-        print("DONE!!!")
 
 
 class CustomData(Data):
@@ -257,69 +208,6 @@ class CustomData(Data):
             return 0
 
 
-# class Graph(Data):
-#     def __init__(self, num_nodes, edges, node_feat, edge_feat):
-#         super().__init__()
-#         self.num_nodes = num_nodes
-#         self.edges = edges
-#         self.node_features = node_feat
-#         self.edge_feat = edge_feat
-#         self.n
-
-
-class GeoPredCollateFn:
-    def __init__(self):
-        ...
-
-    def __call__(self, batch_data_list):
-        atom_bond_graph_list = []
-        bond_angle_graph_list = []
-        angle_dihedral_graph_list = []
-        masked_atom_bond_graph_list = []
-        masked_bond_angle_graph_list = []
-        masked_angle_dihedral_graph_list = []
-        Cm_node_i = []
-        Cm_context_id = []
-        Fg_morgan = []
-        Fg_daylight = []
-        Fg_maccs = []
-        Ba_node_i = []
-        Ba_node_j = []
-        Ba_node_k = []
-        Ba_bond_angle = []
-        Adi_node_a = []
-        Adi_node_b = []
-        Adi_node_c = []
-        Adi_node_d = []
-        Adi_angle_dihedral = []
-        Bl_node_i = []
-        Bl_node_j = []
-        Bl_bond_length = []
-        Ad_node_i = []
-        Ad_node_j = []
-        Ad_atom_dist = []
-
-        bo_bond_order = []
-        atom_cm5 = []
-        atom_espc = []
-        atom_hirshfeld = []
-        atom_npa = []
-
-        graph_dict = {}
-
-        for data in batch_data_list:
-            ab_g = Data(edge_index=data['edges'], x=data['node_feat'], edge_attr=data['edge_attr'])
-            ba_g = Data(edge_index=data['BondAngleGraph_edges'], edge_attr=data['bond_angle'])
-            adi_g = Data(edge_index=data['AngleDihedralGraph_edges'], edge_attr=data['dihedral_angle'])
-
-            atom_bond_graph_list.append(ab_g)
-            bond_angle_graph_list.append(ba_g)
-            angle_dihedral_graph_list.append(adi_g)
-
-        atom_bond_graph, slices = self.collate(atom_bond_graph_list)
-        graph_dict['atom_bond_graph'] = atom_bond_graph
-
-
 if __name__ == "__main__":
     import json
     from tqdm import tqdm
@@ -330,7 +218,8 @@ if __name__ == "__main__":
     dataset = EgeognnPretrainedDataset(
         root='../data/demo', dataset_name='demo',
         remove_hs=True, base_path="../data/demo",
-        atom_names=configs["atom_names"], bond_names=configs["bond_names"], with_provided_3d=False
+        atom_names=configs["atom_names"], bond_names=configs["bond_names"],
+        with_provided_3d=False, mask_value=-2, mask_ratio=0.12
     )
 
     demo_loader = DataLoader(
