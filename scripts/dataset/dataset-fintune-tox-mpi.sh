6d94fdac3c9377433a454ece1dda6b5610663d3c
Kotori Y <yzjkid9@gmail.com>
2024-09-22 17:29:40 +0800

    added shell scripts

---
+++ scripts/dataset/dataset-fintune-tox-mpi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ../..
+
+source /fs1/software/modules/bashrc
+module load loginnode
+module load CUDA/11.8
+module load cudnn/8.4.1-cuda11.x
+export PYTHONUNBUFFERED=1
+
+# root_path="../../../.."
+# export PYTHONPATH="$(pwd)/$root_path/":$PYTHONPATH
+#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6
+
+CONFIG_PATH='./config.json'
+DATASET_ROOT='./data/downstream/toxicity'
+DATASET_BASE_PATH='./data/downstream/toxicity'
+
+SEED=2024
+
+N_NODES=$1
+
+ENDPOINTS="Cat_Intravenous_LD50 Cat_Oral_LD50 Chicken_Oral_LD50 Dog_Oral_LD50 Duck_Oral_LD50 Guineapig_Oral_LD50 Mouse_Intramuscular_LD50 Mouse_Intraperitoneal_LD50 Mouse_Intravenous_LD50 Mouse_Oral_LD50 Mouse_Subcutaneous_LD50 Rabbit_Intravenous_LD50 Rabbit_Oral_LD50 Rat_Inhalation_LC50 Rat_Intraperitoneal_LD50 Rat_Intravenous_LD50 Rat_Oral_LD50 Rat_Skin_LD50 Rat_Subcutaneous_LD50"
+
+echo "run as dataset mode"
+echo "=================="
+mpirun -n $N_NODES python finetune_regr.py \
+  --config-path $CONFIG_PATH \
+  --dataset-root $DATASET_ROOT \
+  --dataset-base-path $DATASET_BASE_PATH \
+  --endpoints $ENDPOINTS \
+  --remove-hs \
+  --useMPI \
+  --dataset
---
+++ scripts/dataset/dataset_mpi.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+cd ../..
+
+source /fs1/software/modules/bashrc
+module load loginnode
+module load CUDA/11.8
+module load cudnn/8.4.1-cuda11.x
+export PYTHONUNBUFFERED=1
+
+# root_path="../../../.."
+# export PYTHONPATH="$(pwd)/$root_path/":$PYTHONPATH
+#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6
+
+CONFIG_PATH='./config.json'
+DATASET_NAME=$1
+DATASET_ROOT='./data/'$DATASET_NAME
+DATASET_BASE_PATH='./data/'$DATASET_NAME
+
+MASK_RATIO=0.15
+
+LATENT_SIZE=256
+ENCODER_HIDDEN_SIZE=1024
+NUM_ENCODER_LAYERS=2
+DROPNODE_RATE=0.1
+ENCODER_DROPOUT=0.1
+NUM_MESSAGE_PASSING_STEPS=2
+
+HIDDEN_SIZE=1024
+NUM_LAYERS=2
+DROPOUT_RATE=0.1
+
+BATCH_SIZE=128
+NUM_WORKERS=72
+LR=1e-4
+BETA=0.999
+WEIGHT_DECAY=1e-2
+
+EPOCHS=100
+LOG_INTERVAL=5
+
+DEVICE='cpu'
+SEED=2024
+
+MODEL_VER='gat'
+
+CHECKPOINT_DIR='./outs/checkpoints/'$DATASET_NAME'-'$MODEL_VER
+
+echo "run as dataset mode"
+echo "=================="
+mpirun -n $2 python pretrain.py \
+  --config-path $CONFIG_PATH \
+  --dataset-root $DATASET_ROOT \
+  --dataset-base-path $DATASET_BASE_PATH \
+  --dataset-name $DATASET_NAME \
+  --mask-ratio $MASK_RATIO \
+  --latent-size $LATENT_SIZE \
+  --encoder-hidden-size $ENCODER_HIDDEN_SIZE \
+  --num-encoder-layers $NUM_ENCODER_LAYERS \
+  --dropnode-rate $DROPNODE_RATE \
+  --encoder-dropout $ENCODER_DROPOUT \
+  --num-message-passing-steps $NUM_MESSAGE_PASSING_STEPS \
+  --hidden-size $HIDDEN_SIZE \
+  --num-layers $NUM_LAYERS \
+  --dropout-rate $DROPOUT_RATE \
+  --checkpoint-dir $CHECKPOINT_DIR \
+  --batch-size $BATCH_SIZE \
+  --num-workers $NUM_WORKERS \
+  --lr $LR \
+  --beta $BETA \
+  --weight-decay $WEIGHT_DECAY \
+  --epochs $EPOCHS \
+  --log-interval $LOG_INTERVAL \
+  --model-ver $MODEL_VER \
+  --device $DEVICE \
+  --lr-warmup \
+  --use-adamw \
+  --remove-hs \
+  --mpi \
+  --dataset
